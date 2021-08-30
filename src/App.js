9b3cad2973f254272dc40f06094e8a506bf2dff9
MilenaCLeme <milenaleme4@hotmail.com>
2021-08-30 18:37:22 -0300

    iniciando a pagina de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router';
+import Login from './pages/Login';
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Route exact path="/" component={ Login } />
+  );
 }
 
 export default App;
