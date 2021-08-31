235f5e7d5b7de2f2307362d9d9a67c6b460497d9
MilenaCLeme <milenaleme4@hotmail.com>
2021-08-30 23:51:03 -0300

    Adionando o reducers do user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ADD_EMAIL:
+    return {
+      ...state,
+      user: action.user,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
